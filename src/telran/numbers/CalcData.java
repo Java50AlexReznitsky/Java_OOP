e6791b73e186290bfceb324acca07c031b3439bb
Barso2 <reznitskyalex@gmail.com>
2023-06-19 02:37:22 +0300

    Updated with shorter solution

--- src/telran/numbers/CalcData.java
+++ src/telran/numbers/CalcData.java
@@ -1,22 +1,22 @@
 package telran.numbers;
 
 public class CalcData {
-	double op1;
-	double op2;
+	double num1;
+	double num2;
 	char operation; //'*','+',...
 	
 	public CalcData(double op1, double op2, char operation) {
-		this.op1 = op1;
-		this.op2 = op2;
+		this.num1 = op1;
+		this.num2 = op2;
 		this.operation = operation;
 	}
 
 	public double getOp1() {
-		return op1;
+		return num1;
 	}
 
 	public double getOp2() {
-		return op2;
+		return num2;
 	}
 
 	public char getOperation() {
--- src/telran/numbers/Calculator.java
+++ src/telran/numbers/Calculator.java
@@ -5,18 +5,12 @@ import java.util.function.BinaryOperator;
 public class Calculator {
 	static public double calculate(CalcData cd) {
 
-		double num1 = cd.op1;
-		double num2 = cd.op2;
-		char oper = cd.operation;
-		char operChar = oper;
-		int asciiCode = (int) operChar;
-
 		DoubleBinaryOperator[] operations = new DoubleBinaryOperator[48];
 		operations[43] = (a, b) -> a + b;
 		operations[45] = (a, b) -> a - b;
 		operations[42] = (a, b) -> a * b;
 		operations[47] = (a, b) -> a / b;
-		return operations[asciiCode].apply(num1, num2);
+		return operations[(int) cd.operation].apply(cd.num1, cd.num2);
 	}
 
 	interface DoubleBinaryOperator extends BinaryOperator<Double> {
